6952f3368ea8b4d4df1b1f75a763c8ff8a07b6de
Stan <s2000ProMax@gmail.com>
2022-04-14 00:27:51 +0300

    feat: optimization refactor code

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import { SheetComponent } from '@core/SheetComponent';
 import { createTable } from '@/components/table/table.template';
-import { $ } from '@core/dom';
+import { resizeHandler } from '@/components/table/table.resize';
+import { shouldResize } from '@/components/table/table.functions';
 
 export class Table extends SheetComponent {
   static className = 'sheet__table';
@@ -16,38 +17,8 @@ export class Table extends SheetComponent {
   }
 
   onMousedown(event) {
-    if (event.target.dataset.resize) {
-      const $resizer = $(event.target);
-      // const $parent = $resizer.$element.parentNode; // bad!!!
-      // const $parent = $resizer.$element.closest('.column'); // bad!!!
-      const $parent = $resizer.closest('[data-type="resizable"]');
-      const coords = $parent.getCoords();
-      const type = $resizer.data.resize;
-      console.log(type)
-
-      const cells = this.$root.findAll(`[data-col="${$parent.data.col}"]`);
-
-      document.onmousemove = (e) => {
-        if (type === 'col') {
-          const deltaX = e.pageX - coords.right;
-          const value = coords.width + deltaX;
-          $parent.css({
-            width: value + 'px'
-          });
-
-          cells.forEach(el => el.style.width = value + 'px');
-        } else if (type === 'row') {
-          const deltaY = e.pageY - coords.bottom;
-          const value = coords.height + deltaY;
-          $parent.css({
-            height: value + 'px'
-          });
-        }
-      }
-
-      document.onmouseup = () => {
-        document.onmousemove = null;
-      }
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
     }
   }
 }
